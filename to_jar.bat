73af769ab7703c9538fc179ecb535908692eef14
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2024-05-07 10:41:49 +0300

    jar packaging script

---
+++ to_jar.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "work_dir=C:\Users\itu\Documents\S4\web-dynamique\winter-framework"
+set "src=%work_dir%\src"
+set "lib=%work_dir%\lib"
+set "bin=%work_dir%\classes"
+set "jar_name=winter-framework"
+set "jar_path=%work_dir%\%jar_name%.jar"
+
+if exist "%bin%" (
+    rd /s /q "%bin%"
+)
+
+:: Java files compilation
+dir /s /B "%src%\*.java" > sources.txt
+javac -d "%bin%" -cp "%lib%\*" @sources.txt
+del sources.txt
+
+:: Jar packaging
+echo Packaging %jar_name%.jar...
+jar cf "%jar_path%" -C "%bin%" .
+
+echo JAR packaging completed
+pause
