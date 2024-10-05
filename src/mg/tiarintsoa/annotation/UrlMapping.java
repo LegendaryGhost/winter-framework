199594e71c9550e7802e6ef02231066410e49316
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2024-10-05 18:33:06 +0300

    Annotation version

--- src/mg/tiarintsoa/annotation/UrlMapping.java
+++ src/mg/tiarintsoa/annotation/UrlMapping.java
@@ -1,7 +1,5 @@
 package mg.tiarintsoa.annotation;
 
-import mg.tiarintsoa.enumeration.RequestVerb;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
