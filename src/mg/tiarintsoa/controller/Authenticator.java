666ae1f2cb6053a4252d81ba681361107c337ce4
Tiarintsoa <rihantiana000@gmail.com>
2025-01-08 14:33:24 +0300

    Empty authentication

--- src/mg/tiarintsoa/controller/Authenticator.java
+++ src/mg/tiarintsoa/controller/Authenticator.java
@@ -8,15 +8,21 @@ import java.util.Arrays;
 
 public class Authenticator {
 
-    public static boolean isAuthorised(HttpServletRequest request, Authenticated authenticated) {
+    public static boolean isAuthorised(HttpServletRequest request, Authenticated authenticatedAnnotation) {
 	HttpSession session = request.getSession();
+
+	Object authenticatedObject = session.getAttribute("authenticated");
+	if (authenticatedObject == null) return false;
+
+	boolean authenticated = (boolean) authenticatedObject;
+	if (authenticatedAnnotation.roles().length == 0) return authenticated;
 
 	Object roleObject = session.getAttribute("role");
 	if (roleObject == null) return false;
 
 	String role = roleObject.toString();
-	String[] authorisedRoles = authenticated.roles();
-	return Arrays.asList(authorisedRoles).contains(role);
+	String[] authorisedRoles = authenticatedAnnotation.roles();
+	return authenticated && Arrays.asList(authorisedRoles).contains(role);
     }
 
 }
